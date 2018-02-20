754d915a59f9a8c618200152e6639b2f59ac2298
yuya yano <yuya.yano.6260@gmail.com>
2018-02-20 22:46:14 +0900

    Test PWA & ph-pages.

--- docs/precache-manifest.9a7278de4a960d1f5020bacd9799effc.js
+++ docs/precache-manifest.9a7278de4a960d1f5020bacd9799effc.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "d500ccac871aa1e31c96",
-    "url": "js/vendor.d500ccac.js"
+    "url": "./js/vendor.d500ccac.js"
   },
   {
     "revision": "ce28c628eea246b643c9",
-    "url": "js/manifest.ce28c628.js"
+    "url": "./js/manifest.ce28c628.js"
   },
   {
     "revision": "60a1702f4d87de8c152d",
-    "url": "js/app.60a1702f.js"
+    "url": "./js/app.60a1702f.js"
   },
   {
     "revision": "12d97b25f653d5cdef93e6c98479d48c",
-    "url": "index.html"
+    "url": "./index.html"
   },
   {
     "revision": "60a1702f4d87de8c152d",
-    "url": "css/app.376e4054.css"
+    "url": "./css/app.376e4054.css"
   }
 ];
